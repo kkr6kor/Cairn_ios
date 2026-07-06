080d296aa7b5bfb98cfd9a89b09fb6187b3152a1
kkr6kor <kkr6kor@bosch.com>
2026-07-06 14:01:35 +0530

    Update package.swift

--- package.swift
+++ package.swift
@@ -11,12 +11,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.1"),
+        // USE THE CORRECT, MODERN MAPLIBRE DISTRIBUTION
+        .package(url: "https://github.com/maplibre/maplibre-gl-native-distribution.git", from: "6.2.7")
     ],
     targets: [
         .target(
             name: "Cairn",
             dependencies: [
                 "Alamofire",
+                // Make sure the product name matches the new package
+                .product(name: "MapLibre", package: "maplibre-gl-native-distribution")
             ],
             path: "Cairn"
         ),
